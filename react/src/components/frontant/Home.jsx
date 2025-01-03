98496e7d0e8936426291403f85b03d5b7a1497cd
2580talhahabib <tsab70809@gmail.com>
2025-01-03 20:01:58 +0500

    starting textemonial section

--- react/src/components/frontant/Home.jsx
+++ react/src/components/frontant/Home.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
-
+import Card from 'react-bootstrap/Card';
 import AboutImg from '../../assets/images/about-us.jpg'
+import construction2 from '../../assets/images/construction2.jpg'
+import construction3 from '../../assets/images/construction3.jpg'
+import construction4 from '../../assets/images/construction4.jpg'
+import construction6 from '../../assets/images/construction6.jpg'
+import icon1 from '../../assets/images/icon-1.svg'
+import icon2 from '../../assets/images/icon-2.svg'
+import icon3 from '../../assets/images/icon-3.svg'
+
 import Header from '../common/Header';
 import Footer from '../common/Footer';
 const Home = () => {
@@ -41,15 +49,175 @@ const Home = () => {
     </div>
 </section>
 {/* our services  */}
-<section className="section-3 ">
-  <div className="container-fluid bg-light py-5">
+<section className="section-3  ">
+  <div className="container-fluid bg-light py-5 ">
     <div className="section-header text-center py-5">
       <span>Our services</span>
       <h2>our construction services</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, vitae neque quasi nam suscipit nihil?</p>
     </div>
-
+<div className="row">
+     <div className="cart col-md-3 col-lg-3 ">
+    <div className="image " >
+     <img src={construction2} alt="" className='w-100 '/>
+    </div>
+    <div className="content">
+      <h4>Specialty Construction</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem, ipsum dolor  elit.</p>
+      <button>READ MORE</button>
+    </div>
+   </div>
+   {/* - */}
+   <div className="cart col-md-3 col-lg-3 ">
+    <div className="image " >
+     <img src={construction3} alt="" className='w-100 '/>
+    </div>
+    <div className="content">
+      <h4>Civil Construction</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem, ipsum dolor  elit.</p>
+      <button>READ MORE</button>
+    </div>
+   </div>
+    {/* - */}
+    <div className="cart col-md-3 col-lg-3 ">
+    <div className="image " >
+     <img src={construction4} alt="" className='w-100 '/>
+    </div>
+    <div className="content">
+      <h4>Residential Construction</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem, ipsum dolor  elit.</p>
+      <button>READ MORE</button>
+    </div>
+   </div>
+    {/* - */}
+    <div className="cart col-md-3 col-lg-3 ">
+    <div className="image " >
+     <img src={construction6} alt="" className='w-100 '/>
+    </div>
+    <div className="content">
+      <h4>Corporate Construction</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem, ipsum dolor  elit.</p>
+      <button>READ MORE</button>
+    </div>
+   </div>
+</div>
   </div>
+</section>
+{/* why chose us  */}
+<div className="section-4 py-5">
+  <div className="container py-5">
+  <div className="section-header text-center py-5">
+      <span>Why Chose Us</span>
+      <h2>Discover our wide variety of projects</h2>
+      <p>Created in close partnership with our clients and collaborators.this approach merges industry experties.<br/>dedicate of our experience innovation and flexibility</p>
+    </div>
+    <div className="row m-5">
+      <div className="col-lg-4 ">
+      <Card style={{ width: '20rem', borderRadius:'3em' ,cursor:'pointer' }}>
+      <Card.Body>
+        <div className="cardicon">
+          <img src={icon1} alt="" className='p-1' />
+        </div>
+        <Card.Title className=''>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.we were add some text on this card
+        </Card.Text>
+      
+      </Card.Body>
+    </Card>
+      </div>
+      <div className="col-lg-4  ">
+      <Card style={{ width: '20rem', borderRadius:'3em' ,cursor:'pointer' }}>
+      <Card.Body>
+        <div className="cardicon">
+          <img src={icon2} alt="" className='p-1' />
+        </div>
+        <Card.Title className=''>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.we were add some text on this card
+        </Card.Text>
+      
+      </Card.Body>
+    </Card>
+      </div>
+      <div className="col-lg-4 ">
+      <Card style={{ width: '20rem', borderRadius:'3em' ,cursor:'pointer' }}>
+      <Card.Body>
+        <div className="cardicon">
+          <img src={icon3} alt="" className='p-1' />
+        </div>
+        <Card.Title className=''>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.we were add some text on this card
+        </Card.Text>
+      
+      </Card.Body>
+    </Card>
+      </div>
+    </div>
+  </div>
+</div>
+{/* our projects  */}
+<section className="section-3  ">
+  <div className="container-fluid bg-light py-5 ">
+    <div className="section-header text-center py-5">
+      <span>Our Projects</span>
+      <h2>our construction services</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, vitae neque quasi nam suscipit nihil?</p>
+    </div>
+<div className="row">
+     <div className="cart col-md-3 col-lg-3">
+    <div className="image " >
+     <img src={construction2} alt="" className='w-100 '/>
+    </div>
+    <div className="content">
+      <h4>Faisalabad Porject</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem, ipsum dolor  elit.</p>
+      <button>READ MORE</button>
+    </div>
+   </div>
+   {/* - */}
+   <div className="cart col-md-3 col-lg-3 ">
+    <div className="image " >
+     <img src={construction3} alt="" className='w-100 '/>
+    </div>
+    <div className="content">
+      <h4>Lahore Porject</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem, ipsum dolor  elit.</p>
+      <button>READ MORE</button>
+    </div>
+   </div>
+    {/* - */}
+    <div className="cart col-md-3 col-lg-3 ">
+    <div className="image " >
+     <img src={construction4} alt="" className='w-100 '/>
+    </div>
+    <div className="content">
+      <h4>Karchi Porject</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem, ipsum dolor  elit.</p>
+      <button>READ MORE</button>
+    </div>
+   </div>
+    {/* - */}
+    <div className="cart col-md-3 col-lg-3 ">
+    <div className="image " >
+     <img src={construction6} alt="" className='w-100 '/>
+    </div>
+    <div className="content">
+      <h4>Paris Project</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem, ipsum dolor  elit.</p>
+      <button>READ MORE</button>
+    </div>
+   </div>
+</div>
+  </div>
+</section>
+
+<section className='section-5'>
+
 </section>
   </main>
 <Footer/>
